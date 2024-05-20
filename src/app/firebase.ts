db7f486f91448c697454cdff8259c499949b8591
sam-ayo <sam-ayo>
2024-05-20 19:26:22 -0230

    fix env to use next env convention

--- src/app/firebase.ts
+++ src/app/firebase.ts
@@ -5,9 +5,12 @@ import * as dotenv from "dotenv"
 dotenv.config()
 
 // TODO: Replace the following with your app's Firebase project configuration
+
 const firebaseConfig: FirebaseOptions = {
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET
-};
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET
+}
+
+console.log('firebase config', firebaseConfig)
 
 const app = initializeApp(firebaseConfig);
 export const firebaseStorage = getStorage(app);
