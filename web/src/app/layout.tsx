77f6e40d1b3723591fbd11bfc4ac52b8d2e093af
sam-ayo <sam-ayo>
2024-06-10 16:53:24 -0230

    add title and description metadata

--- web/src/app/layout.tsx
+++ web/src/app/layout.tsx
@@ -1,31 +1,39 @@
-import "@/styles/globals.css"
-import { Inter as FontSans } from "next/font/google"
-import { cn } from "@/lib/utils"
+import "@/styles/globals.css";
+import { Inter as FontSans } from "next/font/google";
+import { cn } from "@/lib/utils";
 import { SessionProvider } from "next-auth/react";
 import Main from "@/app/main";
 
 const fontSans = FontSans({
   subsets: ["latin"],
   variable: "--font-sans",
-})
+});
 
-export default function RootLayout({ children }: Readonly<{
+export const metadata = {
+  title: "Ragask",
+  description:
+    "AI copilot for researchers and students navigating complex information landscapes",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
   children: React.ReactNode;
-}>)  {
+}>) {
   return (
     <html lang="en" suppressHydrationWarning>
       <head />
-    <link rel="icon" href="/favicon.ico" sizes="any" />
+      <link rel="icon" href="/favicon.ico" sizes="any" />
       <body
         className={cn(
           "min-h-screen bg-background font-sans antialiased",
           fontSans.variable
         )}
       >
-      <SessionProvider>
-      <Main>{children}</Main>
-      </SessionProvider>
+        <SessionProvider>
+          <Main>{children}</Main>
+        </SessionProvider>
       </body>
     </html>
-  )
+  );
 }
