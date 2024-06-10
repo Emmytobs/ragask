b90fe7bd52b50c9a0124500e8be7d8f54972c176
sam-ayo <sam-ayo>
2024-06-10 12:50:14 -0230

    push some auth changes to fix mismatch redirect_uri bug

--- web/src/auth.ts
+++ web/src/auth.ts
@@ -1,4 +1,4 @@
-import NextAuth, { JWT, Session } from "next-auth";
+import NextAuth, {  Session } from "next-auth";
 import Google from "next-auth/providers/google";
 import { IUser } from "@/session";
 import axios from "axios";
@@ -10,47 +10,12 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
   secret: process.env.NEXTAUTH_SECRET,
   callbacks: {
     async jwt({ token, account }) {
-      const customToken = token as JWT;
-      const expires_at = customToken.expires_at as number;
       if (account) {
-        const { access_token, id_token, expires_at, refresh_token } = account;
-        customToken.accessToken = access_token;
-        customToken.jwt = id_token;
-        customToken.expires_at = expires_at;
-        customToken.refresh_token = refresh_token;
-        return token;
-    } else if ( Date.now() < expires_at * 1000) {
+        token.accessToken = account.access_token;
+        token.jwt = account.id_token;
+      }
       return token;
-    }else{
-      if(!customToken.refresh_token){ 
-        throw new Error("No refresh token");
-      }
-      try {
-        const response = await fetch("https://oauth2.googleapis.com/token", {
-          headers: { "Content-Type": "application/x-www-form-urlencoded" },
-          body: new URLSearchParams({
-            client_id: process.env.AUTH_GOOGLE_ID!,
-            client_secret: process.env.AUTH_GOOGLE_SECRET!,
-            grant_type: "refresh_token",
-            refresh_token: customToken.refresh_token!,
-          }),
-          method: "POST",
-        })
-        const responseTokens = await response.json()
-        if (!response.ok) throw responseTokens
-
-        return {
-          ...token,
-          access_token: responseTokens.access_token,
-          expires_at: Math.floor(Date.now() / 1000 + (responseTokens.expires_in as number)),
-          refresh_token: responseTokens.refresh_token ?? token.refresh_token,
-        }
-      } catch (error) {
-        console.error("Error refreshing access token", error)
-        return { ...token, error: "RefreshAccessTokenError" as const }
-      }
-    }
-  },
+    },
     async session({ session, token, user }) {
       if (cachedSession && cachedSession.user?.email === session.user?.email) {
         return cachedSession;
--- web/src/types/next-auth.d.ts
+++ web/src/types/next-auth.d.ts
@@ -5,14 +5,6 @@ declare module 'next-auth' {
   interface Session {
     jwt: string;
     user: IUser;
-    error?: "RefreshAccessTokenError"
-  }
-  interface JWT {
-    accessToken?: string;
-    jwt?: string;
-    expires_at?: number;
-    refresh_token?: string; 
-    error?: "RefreshAccessTokenError"
   }
 }
 
